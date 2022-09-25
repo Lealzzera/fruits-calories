d6da02a3a06efdec03e868dff378b7cf62cd19e9
Lealzzera <matheus.mleall@gmail.com>
2022-09-25 14:44:49 -0300

    fixing types

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -4,10 +4,25 @@ import './Home.css';
 import { api } from '../../api/api';
 import { Link } from 'react-router-dom';
 
+type FruitsData = [
+  {
+    name: string,
+    blubber: string,
+    calories: string,
+    carbohydrates: string,
+    fiber: string,
+    photo: string,
+    portion: string,
+    protein: string
+  }
+]
+
+
+
 const Home = () => {
 
-  const [fruitsData, setFruitsData] = React.useState<any>([]);
-  const [loading, setLoading] = React.useState<any>(null);
+  const [fruitsData, setFruitsData] = React.useState<FruitsData | []>([]);
+  const [loading, setLoading] = React.useState<string | null>(null);
 
   React.useEffect(() => {
     (async () => {
@@ -32,7 +47,7 @@ const Home = () => {
   return (
     <div className='home'>
       <div className='content-home'>
-        {fruitsData.map(({ name, calories, protein, carbohydrates, fiber, portion, photo, blubber }: any, index: any) => (
+        {fruitsData.map(({ name, calories, protein, carbohydrates, fiber, portion, photo, blubber }: any, index: number) => (
           <div key={index}>
             <Link style={{ textDecoration: 'none' }} to="/details">
               <CardFruits name={name} calories={calories} protein={protein} carbo={carbohydrates} fiber={fiber} fat={blubber} portion={portion} image={photo} />
